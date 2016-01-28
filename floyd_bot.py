a2516d545298a3a902bf41eb183246c00a3cec87
kaustuv deolal <vutsuak96@gmail.com>
2016-01-28 11:11:55 +0530

    tracks added

--- floyd_bot.py
+++ floyd_bot.py
@@ -1,6 +1,8 @@
 __name__ = "vutsuak"
 
 from telegram import Updater
+import os
+
 
 updater = Updater(token='142198266:AAHMqzQ9fY_hzu3EOhsiv0Gi0z8Qz5x1034')
 
@@ -32,7 +34,7 @@ def album(bot, update, args):
             flg = 1
             continue
         if flg == 1 and i != '':
-            track += i+"\n"
+            track += i + "\n"
         if flg == 1 and i == '':
             break
 
@@ -40,5 +42,31 @@ def album(bot, update, args):
 
 
 dispatcher.addTelegramCommandHandler('albumname', album)
+
+
+def track_send(bot, update, args):
+    track = str(" ".join(args).lower()).strip()  # take track name as the argument
+    dir = 'E:\PinkFloyd\Pink Floyd'
+    ct = 0
+    list = ['E:\PinkFloyd\Pink Floyd\Pink Floyd - The Dark Side of the Moon',
+            'E:\PinkFloyd\Pink Floyd\Pink Floyd Wish You Were Here']
+    for sub_dir in os.listdir(dir):
+        sub_dir = 'E:\PinkFloyd\Pink Floyd' + '\\' + sub_dir
+
+        for files in os.listdir(sub_dir):
+            Files = sub_dir + "\\"+files
+            if sub_dir in list:
+                files = files.lower().strip()[3:]
+            else:
+                files = files.lower().strip()[5:]
+            filename, ext = os.path.splitext(files)
+            if ext == '.mp3':
+                if track == files[:files.index('.')]:
+                    print Files
+                    bot.sendAudio(chat_id=update.message.chat_id,
+                                  audio=open(Files, 'rb'))
+
+
+dispatcher.addTelegramCommandHandler('track', track_send)
 
 updater.start_polling()
